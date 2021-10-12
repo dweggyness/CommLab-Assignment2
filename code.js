fb4f927ece34fca18c9fc00fe95ce02cd0b2326c
dweggy <colacolaojm@hotmail.com>
2021-10-13 02:46:05 +0400

    complete panel 1 and 2

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ imagesToBePreloaded = [
 ]
 
 document.addEventListener("DOMContentLoaded", function() {
-  preload(imagesToBePreloaded);
+  initEventListeners();
 
   const panelSnapOptions = {
     container: document.body,
@@ -17,11 +17,16 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   const panelSnap = new PanelSnap(panelSnapOptions);
 
+  var reachedFirstRow = false;
   var reachedFinalPanel = false;
   
   panelSnap.on("activatePanel", ( panel ) => {
     if ($(panel).hasClass('row1')) {
-      addPanelHoverEventListeners();
+      if (!reachedFirstRow) { // only run this script once
+        console.log('hello');
+        reachedFirstRow = true;
+        disableScroll();
+      }
     }
     // check if user has scrolled to final panel, if so, disable scrolling and start tracking user clikcs
     if ($(panel).hasClass('row4')) {
@@ -36,14 +41,58 @@ document.addEventListener("DOMContentLoaded", function() {
   // start by snapping to title
   $("body").css("overflow", "visible");
   panelSnap.snapToPanel($(".title")[0]);
+
+  preload(imagesToBePreloaded);
 });
 
-function addPanelHoverEventListeners() {
-  $(".row1 .panel1").hover(() => {
-    $(".row1 .panel1").addClass('unblur')
+function initEventListeners() {
+  setupPanel1();
+}
+
+// set up each panel to be viewed in sequence, they're all separate because
+// you'll only be allowed to unblur the 2nd panel
+// after hovering and completing the animation for the first one ( if any )
+
+function setupPanel1() {
+  const panel = $(".row1 > .panel1")[0];
+
+  $(panel).hover(() => {
+    // on hover, unblur the image and add the text after 0.75s
+    $(panel).addClass('unblur')
+    setTimeout(() => {
+      $('<img id="panel1Text" src="images/Panel1_TextA.png"/>').appendTo($(panel));
+    }, 750)
+  
+    setupPanel2();
   })
-  $(".row1 .panel2").hover(() => {
-    $(".row1 .panel2").addClass('unblur')
+}
+
+function setupPanel2() {
+  const panel = $(".row1 > .panel2")[0];
+
+  $(panel).hover(() => {
+    $(panel).addClass('unblur')
+    setTimeout(() => {
+      $('<img id="panel2TextB" src="images/Panel2_TextB.png"/>').appendTo($(panel));
+      setTimeout(() => {
+        $('<img id="panel2TextC" src="images/Panel2_TextC.png"/>').appendTo($(panel));
+      }, 1500)
+    }, 750)
+
+    enableScroll();
+    setupPanel3();
+  })
+}
+
+function setupPanel3() {
+  $(".row2 .panel1").hover(() => {
+    $(".row2 .panel1").addClass('unblur')
+  })
+}
+
+function addPanelHoverEventListeners() {
+  $(".row2 .panel2").hover(() => {
+    $(".row2 .panel2").addClass('unblur')
   })
 }
 
@@ -74,6 +123,10 @@ function panel6Script() {
 
 function disableScroll() {
   $("body").css("overflow", "hidden");
+}
+
+function enableScroll() {
+  $("body").css("overflow", "visible");
 }
 
 // preload image function stolen from internet
