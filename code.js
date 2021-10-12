113dd4c0232c48c09076ecf2e4f7a70267fe4448
dweggy <colacolaojm@hotmail.com>
2021-10-13 02:01:59 +0400

    work on panel6

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
 
+imagesToBePreloaded = [
+  'images/Panel6_FlyingAxolotl.png',
+  'images/Panel6_AxolotlFinal.png',
+]
+
 document.addEventListener("DOMContentLoaded", function() {
+  preload(imagesToBePreloaded);
+
   const panelSnapOptions = {
     container: document.body,
     panelSelector: '.comicContainer > section',
@@ -10,15 +17,19 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   const panelSnap = new PanelSnap(panelSnapOptions);
 
+  var reachedFinalPanel = false;
   
   panelSnap.on("activatePanel", ( panel ) => {
     if ($(panel).hasClass('row1')) {
-      addRow1EventListeners();
+      addPanelHoverEventListeners();
     }
     // check if user has scrolled to final panel, if so, disable scrolling and start tracking user clikcs
     if ($(panel).hasClass('row4')) {
-      disableScroll();
-      finalPanelScript();
+      if (!reachedFinalPanel) { // only run this script once
+        reachedFinalPanel = true;
+        disableScroll();
+        panel6Script();
+      }
     }
   })
 
@@ -27,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
   panelSnap.snapToPanel($(".title")[0]);
 });
 
-function addRow1EventListeners() {
+function addPanelHoverEventListeners() {
   $(".row1 .panel1").hover(() => {
     $(".row1 .panel1").addClass('unblur')
   })
@@ -36,15 +47,39 @@ function addRow1EventListeners() {
   })
 }
 
-function finalPanelScript() {
-  var clicks = 0;
+function panel6Script() {
+  var userClicked = false;
   const finalPanel = $(".row4 > .comicPanel")[0];
-  finalPanel.addEventListener("click", function() {
-    clicks++;
-    $(finalPanel).css("opacity", 1 - (clicks * 0.15));
+  $(finalPanel).bind('click', function() {
+    if (!userClicked) { // once user clicked, dont trigger for any further clicks
+      userClicked = true; 
+      // add splash image
+      $('<img id="panel6SplashImage" src="images/Panel6_Splash.png"/>').appendTo($(finalPanel));
+      // add axolotl animation flying after 1.25s, 1s for splash to finish expanding
+      setTimeout(() => {
+        $('<img id="panel6Axolotl" src="images/Panel6_FlyingAxolotl.png"/>').appendTo($(finalPanel));
+        // time taken for axolotl to hit screen, 4s, then change image to splat image
+        setTimeout(() => {
+          $('#panel6Axolotl').attr('src', "images/Panel6_AxolotlFinal.png");
+          // sneaky use of setTimeout to make sure that this is run after changing the image
+          setTimeout(() => {
+            $('#panel6Axolotl').css('height', '800px')
+            $('#panel6Axolotl').addClass('slideAnimation')
+          }, 10);
+        }, 3400);
+      }, 1250)
+    }
   });
 }
 
 function disableScroll() {
   $("body").css("overflow", "hidden");
+}
+
+// preload image function stolen from internet
+// preloads the image, so when you insert it into the DOM with javascript it doesnt need a load time
+function preload(arrayOfImages) {
+  $(arrayOfImages).each(function(){
+      $('<img/>')[0].src = this;
+  });
 }
